de6adb71b3063b4770cbdbe6b23e024851c1da8c
steekam <waynewanyee@gmail.com>
2018-09-30 00:36:52 +0300

    Complete add/edit/delete employee

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -19,6 +19,16 @@
         }
 
         /**
+         * Checks if email is already registered
+         */
+        public function check_edit_email($email,$emp_id){
+            $this->db->where('email',$email);
+            $this->db->where('user_id !=',$emp_id);
+            $result = $this->db->get('users');
+            return $result->num_rows() === 0;
+        }
+
+        /**
          * Add employee details
          */
         public function add_employee($enc_password){
@@ -56,15 +66,22 @@
         }
 
         /**
-         * Get employee details
+         * Query for the employee details view
          */
-        public function get_employees(){
-            $order_column = array(null,"name","email",null,null);
-
+        public function employee_query(){
             $this->db->select("user_id AS id, concat_ws(' ',fname,lname) AS name, email, department.name AS department");
             $this->db->from('users');
             $this->db->join('employee','users.user_id = employee.employee_id','inner');
             $this->db->join('department','employee.department_id = department.id','inner');
+        }
+
+        /**
+         * Get all employee details
+         */
+        public function get_employees(){
+            $order_column = array(null,"name","email",null,null);
+            
+            $this->employee_query();
 
             //Search
             if(isset($_POST['search']['value'])){
@@ -88,10 +105,72 @@
             $query = $this->db->get();
 
             return array(
-                'recordsTotal' => $query->num_rows(),
+                'recordsTotal' => $this->all_records(),
                 'recordsFiltered' => $query->num_rows(),
                 'result' => $query->result()
             );
         }
+
+        /**
+         * Counts all the records in employees view
+         */
+        public function all_records(){
+            $this->employee_query();
+            
+            $query = $this->db->get();
+            return $query->num_rows();
+        }
+
+
+        /**
+         * Gets the details of a single employee
+         */
+        public function get_single_employee($emp_id){
+            $this->db->select("user_id AS id, fname, lname, email, department.id AS department");
+            $this->db->from('users');
+            $this->db->join('employee','users.user_id = employee.employee_id','inner');
+            $this->db->join('department','employee.department_id = department.id','inner');
+            $query = $this->db->where('users.user_id',$emp_id);
+            return $this->db->get()->row_array();
+        }
+
+        /**
+         * Update edited employee details
+         */
+        public function edit_employee(){
+            $data_emp = array(
+                'fname' => $this->input->post('fname'),
+                'lname' => $this->input->post('lname'),
+                'email' => $this->input->post('email'),
+            );
+
+            $this->db->where('user_id',$this->input->post('emp_id'));
+            $details = $this->db->update('users',$data_emp);
+
+            $data_dept = array(
+                'department_id' => $this->input->post('department')
+            );
+            $this->db->where('employee_id',$this->input->post('emp_id'));
+            $dept = $this->db->update('employee',$data_dept);
+
+            return $details && $dept;
+        }
+
+        /**
+         * Deletes the records of a employee
+         */
+        public function delete_employee($emp_id){
+            $this->db->where('user_id',$emp_id);
+            return $this->db->delete('users');
+        }
+
+        /**
+         * Fetch departments
+         */
+        public function fetch_departments(){
+            $result = $this->db->get('department');
+            return $result->result_array();
+        }
+        
 
     }
