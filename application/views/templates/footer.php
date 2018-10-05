ff47afbc762002b4f6fe201e6cb75d48e3e30211
auvagha.jb <jerrybenjamin007@gmail.com>
2018-10-05 18:28:26 +0300

    Updated Login and registr UIs + controllers

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,70 @@
+<!--== Footer Area Start ==-->
+    <section id="footer-area">
+    <!-- Footer Widget Start -->
+    <div class="footer-widget-area">
+    <div class="container">
+    <div class="row">
+    <!-- Single Footer Widget Start -->
+    <div class="col-lg-4 col-md-6">
+    <div class="single-footer-widget">
+    <h2>About Us</h2>
+    <div class="widget-body"> 
+    <img src="<?= base_url("assets/img/logo.png"); ?>" alt="JSOFT">
+    
+    <div class="newsletter-area">
+    <form action="index.html">
+    <input type="email" placeholder="Subscribe Our Newsletter">
+    <button type="submit" class="newsletter-btn"><i class="fa fa-paper-plane"></i></button>
+    </form>
+    </div>
+    
+    </div>
+    </div>
+    </div>
+    <!-- Single Footer Widget End -->
+    
+    <!-- Single Footer Widget Start -->
+    <div class="col-lg-4 col-md-6">
+    <div class="single-footer-widget">
+    <h2>get in touch</h2>
+    <div class="widget-body">
+    <ul class="get-touch">
+    <li><i class="fa fa-map-marker"></i> Nairobi West, Kenya</li>
+    <li><i class="fa fa-mobile"></i> +254 725 127 193</li>
+    <li><i class="fa fa-envelope"></i>steekam@gmail.com</li>
+    </ul>
+    <a href="https://goo.gl/maps/b5mt45MCaPB2" class="map-show" target="_blank">Show Location</a>
+    </div>
+    </div>
+    </div>
+    <!-- Single Footer Widget End -->
+    </div>
+    </div>
+    </div>
+    <!-- Footer Widget End -->
+    
+    <!-- Footer Bottom Start -->
+    <div class="footer-bottom-area">
+    <div class="container">
+    <div class="row">
+    <div class="col-lg-12 text-center">
+    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="far fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+    </div>
+    </div>
+    </div>
+    </div>
+    <!-- Footer Bottom End -->
+    </section>
+    <!--== Footer Area End ==-->
+    
+    <!--== Scroll Top Area Start ==-->
+    <div class="scroll-top"> 
+    <img src="<?= base_url("assets/img/scroll-top.png"); ?>" alt="JSOFT">
+    </div>
+    <!--== Scroll Top Area End ==-->
+
 <!--=======================Javascript============================-->
     <!--=== Jquery Min Js ===-->
     <script src="<?= base_url("assets/js/jquery-3.2.1.min.js"); ?>"></script>
@@ -26,12 +93,11 @@
     <!--=== YtPlayer Min Js ===-->
     <script src="<?= base_url("assets/js/plugins/mb.YTPlayer.js"); ?>"></script>
     <!--=== Font-awesome ===-->
-    <script src="assets/js/all.js"></script>
+    <script src="<?= base_url("assets/js/all.js"); ?>"></script>
     <!--=== Magnific Popup Min Js ===-->
     <script src="<?= base_url("assets/js/plugins/magnific-popup.min.js"); ?>"></script>
     <!--=== Slicknav Min Js ===-->
     <script src="<?= base_url("assets/js/plugins/slicknav.min.js"); ?>"></script>
-    
     <!--=== Main Js ===-->
     <script src="<?= base_url("assets/js/main.js"); ?>"></script>
     </body>
