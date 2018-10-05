ddb525d84383787c0a308ba55d0182dec4abe94c
auvagha.jb <jerrybenjamin007@gmail.com>
2018-10-05 10:54:54 +0300

    Added missing icons; trimmed homepage

--- application/views/login.php
+++ application/views/login.php
@@ -1,43 +1,43 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<!DOCTYPE html>
+<?php // defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!--<!DOCTYPE html>
 <html class="no-js" lang="zxx">
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--=== Favicon ===-->
+    === Favicon ===
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 
     <title>Login</title>
 
-    <!--=== Bootstrap CSS ===-->
+    === Bootstrap CSS ===
     <link href="" rel="stylesheet">
-    <!--=== Slicknav CSS ===-->
+    === Slicknav CSS ===
     <link href="assets/css/plugins/slicknav.min.css" rel="stylesheet">
-    <!--=== Magnific Popup CSS ===-->
+    === Magnific Popup CSS ===
     <link href="assets/css/plugins/magnific-popup.css" rel="stylesheet">
-    <!--=== Owl Carousel CSS ===-->
+    === Owl Carousel CSS ===
     <link href="assets/css/plugins/owl.carousel.min.css" rel="stylesheet">
-    <!--=== Gijgo CSS ===-->
+    === Gijgo CSS ===
     <link href="assets/css/plugins/gijgo.css" rel="stylesheet">
-    <!--=== FontAwesome CSS ===-->
+    === FontAwesome CSS ===
     <link href="assets/css/font-awesome.css" rel="stylesheet">
-    <!--=== Theme Reset CSS ===-->
+    === Theme Reset CSS ===
     <link href="assets/css/reset.css" rel="stylesheet">
-    <!--=== Main Style CSS ===-->
+    === Main Style CSS ===
     <link href="style.css" rel="stylesheet">
-    <!--=== Responsive CSS ===-->
+    === Responsive CSS ===
     <link href="assets/css/responsive.css" rel="stylesheet">
 
 
-    <!--[if lt IE 9]>
+    [if lt IE 9]>
         <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <![endif]
 </head>
 
-<body class="loader-active">
+<body class="loader-active">-->
 
     <!--== Preloader Area Start ==-->
     <div class="preloader">
